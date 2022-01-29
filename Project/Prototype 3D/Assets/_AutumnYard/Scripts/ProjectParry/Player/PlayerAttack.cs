64e250126cf1f5d885af5d3b696e55ea68850b4d
autumnyard <autumnyardworks@gmail.com>
2022-01-29 18:15:44 +0100

    Player actions, part 2: Player FSM

--- Project/Prototype 3D/Assets/_AutumnYard/Scripts/ProjectParry/Player/PlayerAttack.cs
+++ Project/Prototype 3D/Assets/_AutumnYard/Scripts/ProjectParry/Player/PlayerAttack.cs
@@ -9,47 +9,41 @@ namespace AutumnYard.ProjectParry
         [SerializeField] private GameObject attack;
         [SerializeField] private GameObject parry;
         [SerializeField] private GameObject defense;
+        [SerializeField] private float parrySpan = .4f; // TODO: El tiempo que aguanta el parry
+        private State _state;
         private bool _isAttacking;
         private bool _isDefending;
         private bool _isParrying;
 
+        public enum State { Blocked, Normal, Attack, Parry, Defense, Hurt }
+
+        private void OnEnable()
+        {
+            _state = State.Normal;
+        }
 
         public void UpdateWithInputs(in PlayerInputs inputs)
         {
             if (inputs.attack)
             {
-                _isAttacking = true;
-                _isParrying = false;
-                _isDefending = false;
+                _state = State.Attack;
             }
             else if (inputs.defensePressed)
             {
-                _isAttacking = false;
-                _isParrying = true;
-                _isDefending = false;
-            }
-            else if (inputs.defenseReleased)
-            {
-                _isAttacking = false;
-                _isParrying = false;
-                _isDefending = false;
+                _state = State.Parry;
             }
             else if (inputs.defenseMaintain)
             {
-                _isAttacking = false;
-                _isParrying = false;
-                _isDefending = true;
+                _state = State.Defense;
             }
             else
             {
-                _isAttacking = false;
-                _isParrying = false;
-                _isDefending = false;
+                _state = State.Normal;
             }
 
-            attack.SetActive(_isAttacking);
-            parry.SetActive(_isParrying);
-            defense.SetActive(_isDefending);
+            attack.SetActive(_state == State.Attack);
+            parry.SetActive(_state == State.Parry);
+            defense.SetActive(_state == State.Defense);
         }
 
         public void EndFrame()
